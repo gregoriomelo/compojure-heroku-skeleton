0ff5fc291df0f8e428c78bf1bd896efb05580fb9
Gregório Melo <gregorio@gregoriomelo.com>
2015-01-22 16:44:56 +1000

    Hello world as JSON

--- src/my_api/core/handler.clj
+++ src/my_api/core/handler.clj
@@ -1,10 +1,12 @@
 (ns my-api.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.util.response :refer [response]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (wrap-json-response (fn [_] (response {:body {:message "Hello World"}}))))
   (route/not-found "Not Found"))
 
 (def app
